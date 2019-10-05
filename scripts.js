6b8fcce799677930ecf3f0d3b6a8bdacefe97f08
duckingwood <duckingwood@gmail.com>
2019-10-04 19:37:23 -0500

    listen for a 'submit' event on the '<form>'

--- scripts.js
+++ scripts.js
@@ -1,6 +1,26 @@
-// Enter a description of this program here.
+// Simple arithmetic calculator app
 
 /**
  * Write some pseudo code steps here.
  * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
  */
+
+function add(a, b) {
+  return Number(a) + Number(b);
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function remainder(a, b) {
+  return a % b;
+}
