a445512c0634b984bc581a645cd3641272c3ef1e
duckingwood <duckingwood@gmail.com>
2019-10-12 19:43:54 -0500

    Use bracket notation 2 invoke appropriate method

--- scripts.js
+++ scripts.js
@@ -1,40 +1,35 @@
 // Simple arithmetic calculator app
 
-/**
- 1. Move Global functions into an object literal.
- 2. Use bracket notation to invoke the appropriate method
- */
-
 const calculator = {
-  add: function add(a, b) {
+  add(a, b) {
     return Number(a) + Number(b);
   },
-  subtract: function subtract(a, b) {
+  subtract(a, b) {
     return a - b;
   },
-  multiply: function multiply(a, b) {
+  multiply(a, b) {
     return a * b;
   },
-  divide: function divide(a, b) {
+  divide(a, b) {
     return a / b;
   },
-  remainder: function remainder(a, b) {
+  remainder(a, b) {
     return a % b;
   }
 };
 
-console.log(calculator.add(1, 2));
+console.log();
 
 const form = document.querySelector("form");
 const num1 = document.querySelector("#num-1");
 const num2 = document.querySelector("#num-2");
 const select = document.querySelector("select");
+const output = document.querySelector("output");
 
 // use an anonymous callback function as the 2nd argument
 form.addEventListener("submit", function(event) {
   // Prevent the default browser behavior DON'T submit
   event.preventDefault();
 
-  // form inputs have a property called value
-  console.log(num1.value, num2.value, select.value);
+  output.textContent = calculator[select.value](num1.value, num2.value);
 });
